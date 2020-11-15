233b56b38d15b4bbec7f1f354596055521e58a67
Siddarth Ijju <dulluijju@gmail.com>
2020-11-14 19:16:49 -0700

    Update hellothere.py

--- hellothere.py
+++ hellothere.py
@@ -4,11 +4,7 @@ import random
 import asyncio
 
 import discord
-#from dotenv import load_dotenv
 from discord.ext import commands
-
-#load_dotenv()
-#TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='$')
 
