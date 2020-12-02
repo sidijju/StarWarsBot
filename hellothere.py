34b10dc30777c10ce12fd53dde89d65be92b7566
sidijju <sidijju@gmail.com>
2020-12-02 09:21:28 -0700

    streamline code

--- hellothere.py
+++ hellothere.py
@@ -24,12 +24,12 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    message_string = message.content.lower()
+    msg = message.content.lower()
     channel = message.channel
     if message.author == bot.user:
         return
 
-    if "$help" == message_string:
+    if "$help" == msg:
         response = ["$openings <movie number or name (1-6)> ---> opening scroll of specified movie",
                     "$random ---> random quote",
                     "$<character name> ---> random quote from character",
@@ -38,11 +38,11 @@ async def on_message(message):
         for resp in response:
             await channel.send(resp)
 
-    elif "alter" in message_string:
+    elif "alter" in msg:
         await channel.send("Pray I don't alter it any further.")
 
-    elif len(message_string) > 0 and '$openings' in message_string:
-        request = message_string[9:].strip()
+    elif len(msg) > 0 and '$openings' in msg:
+        request = msg[9:].strip()
         scrolls = []
         if request in opening_scrolls.keys():
             scrolls = opening_scrolls[request]
@@ -50,19 +50,19 @@ async def on_message(message):
             await channel.send(response)
             await asyncio.sleep(1)
 
-    elif len(message_string) > 0 and message_string[0] == '$':
-        if message_string[1:] == 'random':
+    elif len(msg) > 0 and msg[0] == '$':
+        if msg[1:] == 'random':
             response = random.choice(character_quotes[random.choice(allcharacters)])
         else:
-            key = message_string[1:]
+            key = msg[1:]
             if key in aliases.keys():
                 response = random.choice(character_quotes[aliases[key]])
             else:
                 response = "Invalid input. Use $help for a list of valid commands."
         await channel.send(response)
 
-    elif message_string in responses:
-        for response in responses[message_string]:
+    elif msg in responses:
+        for response in responses[msg]:
             await channel.send(response)
             await asyncio.sleep(1)
 
