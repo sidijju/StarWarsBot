913ba2bfdc81cd2262b2692aa0f257a16540c3d2
sidijju <sidijju@gmail.com>
2023-01-03 21:31:57 -0700

    add new changes

--- hellothere.py
+++ hellothere.py
@@ -2,6 +2,7 @@
 import os
 import random
 import asyncio
+import dotenv
 import re
 
 import discord
@@ -9,75 +10,86 @@ from discord import app_commands
 
 from utilities import *
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-tree = app_commands.CommandTree(client)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+dotenv_file = os.path.join(BASE_DIR, '.env')
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 allcharacters = ["luke", "quigon", "yoda", "hansolo", "obiwan", "darthvader", "macewindu", "darthsidious", "leia", "padme", "jarjar", "hondo", "c3po"]
 
 responses = read_responses()
-aliases = read_character_aliases()
 character_quotes = read_character_quotes(allcharacters)
 opening_scrolls = read_scrolls()
+class StarWarsBot(discord.Client):
+    def __init__(self):
+        intents = discord.Intents.default()
+        #intents.message_content = False
+        super().__init__(intents=intents)
+        self.read_msg = intents.message_content
 
+    async def on_ready(self):
+        await tree.sync()
+        print(f'{self.user.name} has connected to Discord')
+        print(f'Currently connected to {len(self.guilds)} servers')
 
-@client.event
-async def on_ready():
-    await tree.sync(guild=discord.Object(id=Your guild id))
-    print(f'{bot.user.name} has connected to Discord')
-    print(f'Currently connected to {len(bot.guilds)} servers')
+    async def on_message(self, message):
+        if self.read_msg:
+            msg = message.content.lower()
+            channel = message.channel
 
+            if message.author == self.user:
+                return
 
-@tree.command(name = "openings", description = "print the opening scroll of the requested Star Wars movie", guild=discord.Object(id=1060020765736579174))
-async def openings(interaction, ):
-    await interaction.response.send_message("Hello!")
+            elif "alter" in msg:
+                await channel.send("Pray I don't alter it any further.")        
 
-@client.event
-async def on_message(message):
-    msg = message.content.lower()
-    channel = message.channel
+            elif msg in responses:
+                for response in responses[msg]:
+                    await channel.send(response)
+                    await asyncio.sleep(1)
 
-    if message.author == bot.user:
-        return
+bot = StarWarsBot()
+tree = app_commands.CommandTree(bot)
 
-    if "$help" == msg:
-        response = ["$openings <movie number or name (1-6)> ---> opening scroll of specified movie",
-                    "$random ---> random quote",
-                    "$<character name> ---> random quote from character",
-                    "  current characters --> luke, qui gon, yoda, han solo, obi wan, vader, mace windu, sidious, leia, padme, jar jar, c-3po, and hondo ohnaka",
-                    "<quote> ---> response (if in database)"]
-        for resp in response:
-            await channel.send(resp)
+@tree.command(name = "random", description = "print a random quote from a random Star Wars character")
+async def random_quote(interaction: discord.Interaction):
+    response = random.choice(character_quotes[random.choice(allcharacters)])
+    await interaction.response.send_message(response)
 
-    elif "alter" in msg:
-        await channel.send("Pray I don't alter it any further.")
+@tree.command(name = "random-character", description = "print a random quote from chosen Star Wars character")
+@app_commands.choices(name=[app_commands.Choice(name=allcharacters[i], value=allcharacters[i]) for i in range(len(allcharacters))])
+async def random_character_quote(interaction: discord.Interaction, name: app_commands.Choice[str]):
+    response = random.choice(character_quotes[name.value])
+    await interaction.response.send_message(response)
 
-    elif len(msg) > 0 and '$openings' in msg:
-        request = msg[9:].strip()
-        scrolls = []
-        if request in opening_scrolls.keys():
-            scrolls = opening_scrolls[request]
-        for response in scrolls:
-            await channel.send(response)
+@tree.command(name = "openings", description = "print the opening scroll of the requested Star Wars movie (the three trilogies) based on number")
+async def openings(interaction: discord.Interaction, number: app_commands.Range[int, 1, 9]):
+    if number in opening_scrolls.keys():
+        scrolls = opening_scrolls[number]
+        await interaction.response.send_message("Printing scroll")
+        for resp in scrolls:
+            await interaction.followup.send(resp)
             await asyncio.sleep(1)
 
-    elif len(msg) > 0 and msg[0] == '$':
-        if msg[1:] == 'random':
-            response = random.choice(character_quotes[random.choice(allcharacters)])
-        else:
-            key = msg[1:]
-            if key in aliases.keys():
-                response = random.choice(character_quotes[aliases[key]])
-            else:
-                response = "Invalid input. Use $help for a list of valid commands."
-        await channel.send(response)
-
-    elif msg in responses:
-        for response in responses[msg]:
-            await channel.send(response)
+@tree.command(name = "openings-name", description = "print the opening scroll of the requested Star Wars movie (the three trilogies) based on name")
+@app_commands.choices(name=[
+        app_commands.Choice(name="The Phantom Menace", value='1'),
+        app_commands.Choice(name="Attack of the Clones", value='2'),
+        app_commands.Choice(name="Revenge of the Sith", value='3'),
+        app_commands.Choice(name="A New Hope", value='4'),
+        app_commands.Choice(name="The Empire Strikes Back", value='5'),
+        app_commands.Choice(name="Return of the Jedi", value='6'),
+        app_commands.Choice(name="The Force Awakens", value='7'),
+        app_commands.Choice(name="The Last Jedi", value='8'),
+        app_commands.Choice(name="The Rise of Skywalker", value='9')
+        ])
+async def openings_name(interaction: discord.Interaction, name: app_commands.Choice[str]):
+    key = int(name.value)
+    if key in opening_scrolls.keys():
+        scrolls = opening_scrolls[key]
+        await interaction.response.send_message("Printing scroll")
+        for resp in scrolls:
+            await interaction.followup.send(resp)
             await asyncio.sleep(1)
 
-client.run(os.environ['DISCORD_TOKEN'])
+bot.run(os.environ.get('DISCORD_TOKEN'))
